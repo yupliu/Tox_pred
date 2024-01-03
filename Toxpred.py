cb1c0db236a8d2c823aed2e10460e746b319d21d
yupliu <yupliu@users.noreply.github.com>
2024-01-03 14:51:31 -0500

    Fingerprint

--- Toxpred.py
+++ Toxpred.py
@@ -6,3 +6,9 @@ csv_path = "d:\\ChemData\\tox21.csv"
 tox = pd.read_csv(csv_path)
 tox.dropna(inplace=True)
 tox["mol"] = tox["smiles"].apply(Chem.MolFromSmiles)
+
+
+from rdkit.Chem import rdFingerprintGenerator
+import numpy as np
+rd_fpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2,fpSize=2048)
+tox["fp"] = tox["mol"].apply(rd_fpgen.GetFingerprint)
