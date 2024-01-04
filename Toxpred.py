fb7c4ff0effff4ff6e04b900c5750116fa5ba71e
yupliu <yupliu@users.noreply.github.com>
2024-01-04 12:49:54 -0500

    Bit string fingerprint

--- Toxpred.py
+++ Toxpred.py
@@ -3,6 +3,7 @@ import rdkit as rd
 from rdkit import Chem
 
 csv_path = "d:\\ChemData\\tox21.csv"
+csv_path = "C:\\Data\\tox21.csv"
 tox = pd.read_csv(csv_path)
 tox.dropna(inplace=True)
 tox["mol"] = tox["smiles"].apply(Chem.MolFromSmiles)
@@ -12,3 +13,4 @@ from rdkit.Chem import rdFingerprintGenerator
 import numpy as np
 rd_fpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2,fpSize=2048)
 tox["fp"] = tox["mol"].apply(rd_fpgen.GetFingerprint)
+tox["fp_bits"]=tox["fp"].apply(lambda x:x.ToBitString())
